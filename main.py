c20b0af45a544b946b8abfda09a610cdd6c14ff2
sorrowfeng <1399600304@qq.com>
2025-06-26 17:10:01 +0800

    增加命令行

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Hand Gesture Detector")
     parser.add_argument('--camera', '-c', type=int, default=0, help='Camera index (default: 0)')
+    parser.add_argument('--show-window', action='store_true', help='Show the video window (default: False)')
+
     args = parser.parse_args()
 
     detector = HandGestureDetector()
+    detector.set_show_window(args.show_window)  # 设置是否显示窗口
     detector.run(camera_index=args.camera)
